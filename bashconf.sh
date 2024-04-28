b1d420a2e8336561c190de980d14d0ad86cbdbd0
Owen Frederick Dalton <61063602+rohanmodil123@users.noreply.github.com>
2024-04-27 21:26:46 -0400

    Add files via upload

---
+++ bashconf.sh
@@ -0,0 +1,13 @@
+# AutoRun
+# =======
+
+fortune | cowsay
+
+
+# Aliases
+# =======
+
+alias cdsetup='cd ~/Documents/Setup'
+alias viminfo='nvim ~/Documents/Setup/info'
+alias cls='clear'
+alias 'cd..'='cd ..'
---
+++ vimconf.vim
@@ -0,0 +1,16 @@
+" appearance
+
+set relativenumber
+
+
+" vim-plug
+
+call plug#begin('~/.nvim/plugged')
+
+Plug 'lervag/vimtex'
+
+call plug#end()
+
+" vimtex
+
+let g:vimtex_view_method = 'zathura'
